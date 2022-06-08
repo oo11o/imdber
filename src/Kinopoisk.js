eb19eb29de07d568ba0730c40508e03ae7060e2a
Vlad Imir <ymyr.vlad@gmail.com>
2022-06-08 21:23:09 +0300

    feat: add method setYear

--- src/Kinopoisk.js
+++ src/Kinopoisk.js
@@ -1,27 +1,32 @@
 import * as yup from 'yup';
 
 class Kinopoisk {
-  constructor() {
-    this._content = {
-      name: '',
-      year: '',
-    };
-  }
-
-  _setName(content) {
-    this._content.name = Kinopoisk.validation('name', content.trim());
-  }
-
- static validation(key, content) {
-    const schema = {
-      name: yup.string().min(1).max(300),
-    };
-    try {
-      return schema[key].validateSync(content);
-    } catch (e) {
-      throw new Error(`Validation failed: ${e.message}`);
+    constructor() {
+        this._content = {
+            name: '',
+            year: '',
+        };
     }
-  }
+
+    _setName(content) {
+        this._content.name = Kinopoisk.validation('name', content.trim());
+    }
+
+    _setYear(content) {
+        this._content.name = Kinopoisk.validation('year', content);
+    }
+
+    static validation(key, content) {
+        const schema = {
+            name: yup.string().min(1).max(300),
+            year: yup.number().integer().min(1800).max(2030),
+        };
+        try {
+            return schema[key].validateSync(content);
+        } catch (e) {
+            throw new Error(`Validation failed: ${e.message}`);
+        }
+    }
 }
 
 export default Kinopoisk;
