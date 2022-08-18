1b625f3e3ea013cf82692c517137e924acbc3837
Vlad Imir <ymyr.vlad@gmail.com>
2022-08-18 20:07:09 +0300

    test: change checkUrl to static

--- __test__/Parse.test.js
+++ __test__/Parse.test.js
@@ -1,10 +1,8 @@
 import Parse from '../src/Parse';
 
-const parse = new Parse();
-
 describe('parse:', () => {
   test('check url', () => {
-    expect(parse.checkUrl('https://www.imdb.com/title/tt0088247/?ref_=fn_al_tt_1')).toEqual('https://www.imdb.com/title/tt0088247/');
-    expect(parse.checkUrl('tt0088247')).toEqual('https://www.imdb.com/title/tt0088247/');
+    expect(Parse.checkUrl('https://www.imdb.com/title/tt0088247/?ref_=fn_al_tt_1')).toEqual('https://www.imdb.com/title/tt0088247/');
+    expect(Parse.checkUrl('tt0088247')).toEqual('https://www.imdb.com/title/tt0088247/');
   });
 });
